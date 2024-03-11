215c27eae7564aa9a32b14a03a0a64187ab5f975
Mariouz <joseph02252003@gmail.com>
2024-03-12 00:59:23 +0800

    [AUTH] able to get login request from front end

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
 const express = require('express')
+const cors = require('cors')
 const db = require('./db')
 const app = express()
+var bodyParser = require('body-parser')
 const port = 3030
 
+app.use(cors())
+app.use( bodyParser.json() ); 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json());       
+app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.send('Deployed');
@@ -12,6 +19,13 @@ app.get('/cart', (req, res) => {
     var userid = req.query.userid
     const result = db.query('select * from users')
     res.json(result.rows)
+})
+
+app.post('/login', (req, res) => {
+    username = req.body.username;
+    pwd = req.body.password;
+    console.log(username, pwd);
+    res.json({userid: 3});
 })
 
 app.listen(port, (err) => {
