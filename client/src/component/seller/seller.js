9c416b0005626457b9efebed502ae7feb2a6dc71
Mariouz <joseph02252003@gmail.com>
2024-04-03 18:02:52 +0800

    [SELLER] fix bug

--- client/src/component/seller/seller.js
+++ client/src/component/seller/seller.js
@@ -3,31 +3,27 @@ import {useNavigate} from 'react-router-dom'
 import Cookies from 'universal-cookie'
 import axios from 'axios'
 
-const seller_product = async(id) => {
-    var response = await axios.get(`http://localhost:3030/seller_product?id=${id}`);
-    return response.data;
-}
-function FetchProductID(props){
-    const userid = props.userid;
+function FetchProductID(userid, setProducts){
     const [isLoading, setLoading] = useState(true);
-    const [productlist, setlist] = useState();
-    seller_product(userid)
+    const [productlist, setlist] = useState([]);
+    axios.get(`http://localhost:3030/seller_product?id=${userid}`) //fetch seller products product_id
     .then(res => {
-        setlist(res)
-        setLoading(false);
-    });                //fetch seller products product_id
-    if(isLoading) return <p>Loading...</p>;
-    var list = []
-    var L = Object.keys(productlist).length;
-    for(var i=0;i<L;i++) {
-        const cur = productlist[i];
-        console.log(cur['product_id']);
-        list.push(
-            <p>{cur['product_id']}:</p>)
-        list.push(<p>name: {cur['product_name']}, price: {cur['price']}</p>
-        )
-    }
-    return list;
+        setlist(res.data)
+        setLoading(false)
+    })
+    useEffect(() => {
+        var list = []
+        var L = Object.keys(productlist).length;
+        for(var i=0;i<L;i++) {
+            const cur = productlist[i];
+            console.log(cur['product_id']);
+            list.push(
+                <p>{cur['product_id']}:</p>)
+            list.push(<p>name: {cur['product_name']}, price: {cur['price']}</p>
+            )
+        }
+        setProducts(() => {return list})
+    }, [isLoading])
 }
 function Seller(props) {
     
@@ -35,12 +31,14 @@ function Seller(props) {
     const navigate = useNavigate();
     const userid = cookies.get('userid');
     if(!userid) navigate('/login');
+    const [products, setProducts] = useState(() => {return <p>Loading...</p>})
     
     return (
         <div>
             <h1>Seller Page</h1>
             <h1>Inventory:</h1>
-            <FetchProductID userid={userid}/>
+            {FetchProductID(userid, setProducts)}
+            {products}
         </div>
     )
 }
