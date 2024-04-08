4171be5ec3be4bd662fbf5d01981be61d54bad2d
Hohn1161 <hohnhon1161@gmail.com>
2024-04-08 16:39:48 +0800

    add back notification

--- server/index.js
+++ server/index.js
@@ -446,6 +446,33 @@ app.post('/add_product', upload.single('image'), async(req, res) => {
     }
 })
 
+add_noti = async(msg, productid) => {
+    const fetch_user = await db.query(`select user_id from wishlist where product_id = ${productid}`)
+    const userlist = fetch_user.rows
+    const L = Object.keys(userlist).length
+    for(var i=0;i<L;i++){
+        const cur_user_id = userlist[i]['user_id'];
+        const fetch_num_of_noti = await db.query(`select count(*) from noti where user_id = ${cur_user_id}`);
+        const number_of_noti = fetch_num_of_noti.rows[0]['count']
+        if(number_of_noti == 10){
+            await db.query(`delete from noti where create_at = (select create_at from noti order by create_at asc limit 1) `);
+        }
+        await db.query(`insert into noti (user_id, context, product_id) values (${userid}, '${msg}', ${productid})`)
+    }
+}
+
+app.get('/user_notification', async(req, res) => {
+    userid = req.query.userid;
+    try{
+        result = await db.query(`select product_id, context, current_timestamp - noti.create_at as time from noti where user_id = ${userid}`)
+        res.json(result.rows)
+    }
+    catch(err){
+        res.json({'err': err})
+    }
+
+})
+
 app.post('/edit_product', upload.single('image'), async(req, res) => {
     productid = req.body.productid;
     productname = req.body.productname.replace('\'', '\'\'');
