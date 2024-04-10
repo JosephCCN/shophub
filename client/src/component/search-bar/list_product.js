6ea937019570badaf1243af3c86e62b0fb04c726
Mariouz <joseph02252003@gmail.com>
2024-04-10 19:41:18 +0800

    [PRODUCT] list product can choose top

--- client/src/component/search-bar/list_product.js
+++ client/src/component/search-bar/list_product.js
@@ -1,8 +1,11 @@
 import { LoadProduct, LoadProductPhoto} from '../util/product';
+import { useState } from 'react';
 import "./css/list_product.css";
 
 
 function ListProduct(props) {
+
+    const [top, setTop] = useState(10)
 
     const products = props.products;
     const L = Object.keys(products).length;
@@ -11,7 +14,7 @@ function ListProduct(props) {
     if(L == 0) return <h1>There is no product suit your criteria</h1>
     
 
-    for(var i=0;i<L;i++) {
+    for(var i=0;i<L && i < parseInt(top);i++) {
         const cur = products[i];
         const cur_product_id = cur['product_id']
         const entities = ['product_name', 'price']
@@ -27,8 +30,36 @@ function ListProduct(props) {
     }
     
     return (
-        <div className='product_result'>
-            {list}
+        <div>
+            <table>
+                <tr>
+                    <td>
+                    <div className='top'>
+                        <table>
+                            <tr>
+                                <td>
+                                    <label className='top_text'>Top:</label>
+                                </td>
+                                <td>
+                                <select value={top} onChange={(e) => {setTop(e.target.value)}}>
+                                    <option value='10'>10</option>
+                                    <option value='20'>20</option>
+                                    <option value='50'>50</option>
+                                </select>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div className='product_result'>
+                            {list}
+                        </div>
+                    </td>
+                </tr>
+            </table>  
         </div>
     )
 }
